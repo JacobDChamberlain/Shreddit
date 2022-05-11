578b68e11251c781cadc5257eb1529f42fd45eeb
JacobDChamberlain <straightlegmusic@gmail.com>
2022-05-10 19:00:27 -0500

    route - get all communities & create community route & communitywtform

--- app/api/community_routes.py
+++ app/api/community_routes.py
@@ -1,11 +1,35 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import db, Community
-# from app.forms import CommunityForm
+from app.forms import CommunityForm
 
 community_routes = Blueprint('communities', __name__)
+
+@community_routes.route('/')
+def get_all_communities():
+    communities = Community.query.all()
+    # (below is an example, if the order needed to be newest-first)
+    # communities = Community.query.order_by(Community.id.desc()).all()
+    return jsonify([community.to_dict() for community in communities])
 
 @community_routes.route('/<int:id>')
 def get_one_community(id):
     community = Community.query.get(id)
     return community.to_dict()
+
+@community_routes.route('/', methods=['POST'])
+def create_community():
+    form = CommunityForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        community = Community(
+            name = form.data['name'],
+            description = form.data['description'],
+            community_pic = form.data['community_pic'],
+            category = form.data['category'],
+            user_id = form.data['user_id']
+        )
+        db.session.add(community)
+        db.session.commit()
+        return community.to_dict()
---
+++ app/forms/community_form.py
@@ -0,0 +1,12 @@
+from random import choices
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Community
+
+class CommunityForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    community_pic = StringField('Community Pic')
+    category = SelectField('Category', choices=["Classical", "Jazz", "Hair Metal", "Virtuoso", "Speed Metal", "Bluegrass", "Metal", "Country", "Death Metal"], validators=[DataRequired()])
+    user_id = IntegerField('User Id', validators=[DataRequired()])
