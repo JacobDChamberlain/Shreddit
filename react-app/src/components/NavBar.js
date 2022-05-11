9dc6b09af462ca4b1e7bd0ae2ba47b65c7010652
JacobDChamberlain <straightlegmusic@gmail.com>
2022-05-11 15:44:50 -0500

    added demo button & refactored nav bar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,28 +1,49 @@
-
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useHistory } from 'react-router-dom';
+import { login } from '../store/session';
 import LogoutButton from './auth/LogoutButton';
 
+
 const NavBar = () => {
+
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const currentUser = useSelector(state => state.session.user)
+
+  const handleDemo = async (e) => {
+    e.preventDefault()
+
+    const demoUser = {
+      email: 'demo@aa.io',
+      password: 'password'
+    }
+
+    await dispatch(login(demoUser.email, demoUser.password))
+    history.push('/')
+  }
+
   return (
     <nav>
       <div className='navlinks-container'>
-          <NavLink to='/' exact={true} activeClassName='active'>
+          {currentUser && <NavLink to='/' exact={true} activeClassName='active'>
             Home
-          </NavLink>
-          <NavLink to='/login' exact={true} activeClassName='active'>
+          </NavLink>}
+          {!currentUser &&  <NavLink to='/login' exact={true} activeClassName='active'>
             Login
-          </NavLink>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+          </NavLink>}
+          {!currentUser &&  <button className='demo-button' type='button' onClick={handleDemo}>Demo</button>}
+          {!currentUser && <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
-          </NavLink>
-          <NavLink to='/users' exact={true} activeClassName='active'>
+          </NavLink>}
+          {currentUser && <NavLink to='/users' exact={true} activeClassName='active'>
             Users
-          </NavLink>
-          <NavLink to='/communities' exact={true} activeClassName='active'>
+          </NavLink>}
+          {currentUser && <NavLink to='/communities' exact={true} activeClassName='active'>
             Communities
-          </NavLink>
-          <LogoutButton />
+          </NavLink>}
+          {currentUser && <LogoutButton />}
       </div>
     </nav>
   );
