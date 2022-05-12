5976a6e9f52e74102a455a9deee3b1184f881016
JacobDChamberlain <straightlegmusic@gmail.com>
2022-05-12 18:13:35 -0500

    seeded posts table

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .communities import seed_communities, undo_communities
+from .posts import seed_posts, undo_posts
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_communities()
+    seed_posts()
     # Add other seed functions here
 
 
@@ -20,4 +22,5 @@ def seed():
 def undo():
     undo_users()
     undo_communities()
+    undo_posts()
     # Add other undo functions here
