b8119efb5501d270e4b82c37dadfeef02839de4b
JacobDChamberlain <straightlegmusic@gmail.com>
2022-05-16 16:58:48 -0500

    styled splash page

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import LoginForm from '../auth/LoginForm';
 import SignUpForm from '../auth/SignUpForm';
 import landingBgImg from '../../images/shreddit_landing_bg.png'
+import shlogo from '../../images/shreddit_avatar2.png'
+import shlogo2 from '../../images/shlogo.png'
 import './LandingPage.css'
 
 const LandingPage = () => {
@@ -15,6 +17,7 @@ const LandingPage = () => {
                 </div>
                 <div className='forms-container-right'>
                     <h2>Welcome to Shreddit!</h2>
+                    {showSignUp ? <img src={shlogo2}></img> : <img src={shlogo}></img>}
                     {showSignUp ? <h3>Sign up</h3> : <h3>Log in</h3>}
                     {showSignUp ?
                     <div className='login-form-container'>
@@ -28,7 +31,7 @@ const LandingPage = () => {
                         <LoginForm />
                         <div className='forms-footer'>
                             Don't have an account?
-                            <button className='show-form-button' onClick={() => setShowSignUp(true)}>Sign up</button>
+                            <button className='switch-form-button' onClick={() => setShowSignUp(true)}>Sign up</button>
                         </div>
                     </div>}
                 </div>
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import './SignupForm.css'
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
@@ -48,59 +49,64 @@ const SignUpForm = () => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
+    <form className='signup-form' onSubmit={onSignUp}>
       <div>
         {errors.map((error, ind) => (
           <div className='error-text' key={ind}>{error}</div>
         ))}
       </div>
       <div>
-        <label>User Name</label>
         <input
+          className='input-field'
           type='text'
           name='username'
           onChange={updateUsername}
           value={username}
+          placeholder='Username'
         ></input>
       </div>
       <div>
-        <label>Email</label>
         <input
+          className='input-field'
           type='text'
           name='email'
           onChange={updateEmail}
           value={email}
+          placeholder='Email'
         ></input>
       </div>
       <div>
-        <label>Profile Picture</label>
         <input
+          className='input-field'
           type='text'
           name='profilePic'
           onChange={updateProfilePic}
           value={profilePic}
+          placeholder='Profile Picture (must be .jpg URL)'
         ></input>
       </div>
       <div>
-        <label>Password</label>
         <input
+          className='input-field'
           type='password'
           name='password'
           onChange={updatePassword}
           value={password}
+          placeholder='Password'
         ></input>
       </div>
       <div>
-        <label>Repeat Password</label>
         <input
+          className='input-field'
           type='password'
           name='repeat_password'
           onChange={updateRepeatPassword}
           value={repeatPassword}
           required={true}
+          placeholder='Confirm Password'
         ></input>
       </div>
-      <button type='submit'>Sign Up</button>
+      <button className='login-button' type='submit'>Continue</button>
     </form>
   );
 };
