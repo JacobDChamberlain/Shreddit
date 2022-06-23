6480ac671f3b22a778e4e46c2fcc1d0bc32e450b
JacobDChamberlain <straightlegmusic@gmail.com>
2022-06-23 17:49:10 -0500

    updated helplinks w links to projects, portfolio, & contact

--- react-app/src/components/HelpLinks/HelpLinks.js
+++ react-app/src/components/HelpLinks/HelpLinks.js
@@ -7,18 +7,18 @@ const HelpLinks = () => {
             <div className="help-links-content">
                 <div className="help-links-left">
                     <ul className="help-links-ul">
-                        <li className="help-link">Help</li>
-                        <li className="help-link">Shreddit Coins</li>
-                        <li className="help-link">Shreddit Premium</li>
+                        <li className="help-link">Other Projects:</li>
+                        <li className="help-link"><a className="help-a" href="https://flixtagram.herokuapp.com">Flixtagram</a></li>
+                        <li className="help-link"><a className="help-a" href="https://rememberthemilk-solo-fork.herokuapp.com">MemberTheMilk</a></li>
                     </ul>
                 </div>
                 <div className="help-links-right">
                     <ul className="help-links-ul">
                         <li className="help-link">About</li>
-                        <li className="help-link">Careers</li>
-                        <li className="help-link">Press</li>
-                        <li className="help-link">Advertise</li>
-                        <li className="help-link">Blog</li>
+                        <li className="help-link"><a className="help-a" href="https://jacobdchamberlain.github.io/">Portfolio</a></li>
+                        <li className="help-link"><a className="help-a" href="https://open.spotify.com/artist/0CP5nqR6lT3g3StExsINGG">Press</a></li>
+                        <li className="help-link"><a className="help-a" href="mailto: JacobDChamberlain@gmail.com">Email</a></li>
+                        <li className="help-link"><a className="help-a" href="tel:214-772-4221">Mobile</a></li>
                         <li className="help-link">Terms</li>
                         <li className="help-link">Content Policy</li>
                         <li className="help-link">Privacy Policy</li>
@@ -27,15 +27,15 @@ const HelpLinks = () => {
                 </div>
             </div>
             <div className="help-links-languages">
-                    <div className="help-links-language-left">
-                        <li className="help-link">USA/Global</li>
-                    </div>
-                    <div className="help-links-language-right">
-                        <li className="help-link">Deutsch</li>
-                    </div>
+                {/* <div className="help-links-language-left">
+                    <li className="help-link">USA/Global</li>
+                </div>
+                <div className="help-links-language-right">
+                    <li className="help-link">Deutsch</li>
+                </div> */}
             </div>
             <div className="help-links-footer">
-                Shreddit Inc © 2022. All rights reserved
+                Jacob D. Chamberlain 2022
             </div>
         </div>
     )
