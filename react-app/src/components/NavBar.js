398d7bfd3dcfa7b2924c945f18eccf4c741d4a9e
JacobDChamberlain <straightlegmusic@gmail.com>
2022-07-06 16:06:28 -0500

    added 'Welcome, {username}!' to navbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useHistory } from 'react-router-dom';
-import { login } from '../store/session';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import SearchBar from './SearchBar/SearchBar';
 import { HiHome } from 'react-icons/hi'
@@ -9,25 +8,18 @@ import { HiHome } from 'react-icons/hi'
 
 const NavBar = () => {
 
-  const dispatch = useDispatch()
-  const history = useHistory()
 
   const currentUser = useSelector(state => state.session.user)
 
   return (
-    <nav hidden={currentUser ? false : true}>
+    <nav hidden={ currentUser ? false : true }>
       <div className='navlinks-container'>
-          {currentUser && <NavLink className='home-button-nav' to='/' exact={true} activeClassName='active'>
+          <NavLink className='home-button-nav' to='/' exact={true} activeClassName='active'>
             <HiHome className='icon' /> Home
-          </NavLink>}
-          {/* {!currentUser &&  <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>} */}
-          {currentUser && <SearchBar />}
-          {/* {!currentUser && <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>} */}
-          {currentUser && <LogoutButton />}
+          </NavLink>
+          <SearchBar />
+          <div>Welcome, <NavLink className='welcome-username' to={`/user/${currentUser?.username}/${currentUser?.id}`}>{currentUser?.username}</NavLink>!</div>
+          <LogoutButton />
       </div>
     </nav>
   );
