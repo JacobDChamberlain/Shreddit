9c2a710089254c9d90d98c1e209d09b6968a7afe
JacobDChamberlain <straightlegmusic@gmail.com>
2022-08-03 13:50:30 -0500

    search results page styling

--- react-app/src/components/SearchResults/SearchResultsPage.js
+++ react-app/src/components/SearchResults/SearchResultsPage.js
@@ -5,6 +5,7 @@ import { search, emptySearch } from "../../store/search";
 import HelpLinks from "../HelpLinks/HelpLinks";
 import Post from "../Posts/Post";
 import './SearchResultsPage.css'
+import shlogo from "../../images/shlogo.png"
 
 
 
@@ -35,27 +36,30 @@ const SearchResultsPage = () => {
     return (
         <div className="search-results-page-container">
             <div className="search-results-page-left">
-                <div className="all-results-container">
-                    <ul className="community-results-ul">
-                        {results?.map((community) => (
-                            <li onClick={() => {history.push(`/sh/${community?.name}/${community?.id}`)}} className="community-result-li" key={community?.id}>
-                                {community?.community_pic && <img className="comm-suggestion-pic" src={community?.community_pic}></img>}
-                                <NavLink className='comm-sugg' to={`/sh/${community?.name}/${community?.id}`} key={community?.id}>{community?.name}</NavLink>
-                                <p>{community?.description}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-
+                <ul className="community-results-ul">
+                    {results?.map((community) => (
+                        <li onClick={() => {history.push(`/sh/${community?.name}/${community?.id}`)}} className="community-result-li" key={community?.id}>
+                            {community?.community_pic && <img className="comm-suggestion-pic" src={community?.community_pic}></img>}
+                            <NavLink className='comm-sugg' to={`/sh/${community?.name}/${community?.id}`} key={community?.id}>{community?.name}</NavLink>
+                            <p>{community?.description}</p>
+                        </li>
+                    ))}
+                </ul>
             </div>
-
             <div className="search-results-page-right">
-                <div style={{visibility: "hidden"}} className="community-search-results">
+                <div className="community-suggestions-container">
+                    <div className="sugg-header">
+                        <img className="shlogo" src={shlogo}></img>
+                        <h4 className="sugg-h4">Search for Shredders</h4>
+                    </div>
+                    <p className="prem-p">Discover new guitarists! The shredder world is endlessly growing! Chug chug chug chug Meedly meedly meedly meedly MEEEEEEE</p>
+                </div>
+                {/* <div style={{visibility: "hidden"}} className="community-search-results">
                     - display communities -
                 </div>
                 <div style={{visibility: "hidden"}} className="user-search-results">
                     - display people -
-                </div>
+                </div> */}
                 <HelpLinks />
             </div>
         </div>
