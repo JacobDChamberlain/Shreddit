190a97c588bd7debe4d7cc178673a7ac9954f45c
JacobDChamberlain <straightlegmusic@gmail.com>
2022-08-10 13:33:05 -0500

    not ALL post images should be broken

--- react-app/src/components/Posts/Post.js
+++ react-app/src/components/Posts/Post.js
@@ -111,7 +111,7 @@ const Post = ({ post, communityId }) => {
                         </div>
                         <div className="post-title-div">{post.title}</div>
                     </div>
-                    <img onError={addDefaultImageSrc} className="post-image" src={post.image_url}></img>
+                    <img className="post-image" src={post.image_url} onError={addDefaultImageSrc}></img>
                     <p className="post-body">{post.body}</p>
                 </ul> :
                 <div>
