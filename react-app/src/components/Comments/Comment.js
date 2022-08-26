26aa5308ff9d99b7bb89f40d180647dcca0b4597
JacobDChamberlain <straightlegmusic@gmail.com>
2022-08-25 20:17:26 -0500

    users can now edit their own comments

--- react-app/src/components/Comments/Comment.js
+++ react-app/src/components/Comments/Comment.js
@@ -4,6 +4,8 @@ import { NavLink, useHistory } from "react-router-dom";
 import { updateComment, deleteComment } from "../../store/comments";
 import './Comment.css'
 import { BsTrash } from 'react-icons/bs'
+// import { BsThreeDots } from 'react-icons/bs'
+import { RiEdit2Line } from 'react-icons/ri'
 
 
 const Comment = ({ comment }) => {
@@ -15,6 +17,30 @@ const Comment = ({ comment }) => {
 
     const dispatch = useDispatch();
 
+    const [validationErrors, setValidationErrors] = useState([]);
+    const [showEditComment, setShowEditComment] = useState(false);
+    const [content, setContent] = useState(comment.content);
+    const userId = currentUser.id;
+    const postId = comment.post_id;
+
+
+    const handleEditComment = async (e) => {
+        e.preventDefault();
+        const editedComment = {
+            id: comment.id,
+            content: content,
+            user_id: userId,
+            post_id: postId
+        }
+        const data = await dispatch(updateComment(editedComment))
+        if (data) {
+            setValidationErrors(data)
+        } else {
+            setValidationErrors([])
+            setShowEditComment(false);
+        }
+        setContent('')
+    }
 
     const handleDeleteComment = async () => {
         await dispatch(deleteComment(comment.id))
@@ -28,11 +54,35 @@ const Comment = ({ comment }) => {
                     <NavLink className='user-link-comment' to={`/user/${comment.username}/${comment.user_id}`}>/u/{comment.username}</NavLink>
                     <div className="comment-date">{moment.tz(comment.created_at, 'America/Chicago').fromNow()}</div>
                 </div>
-                {currentUser.id === comment.user_id && <BsTrash onClick={handleDeleteComment} className="trash-icon" />}
+                {currentUser.id === comment.user_id &&
+                <div className="edit-delete-comment-buttons-container">
+                    <BsTrash onClick={handleDeleteComment} className="trash-icon" />
+                    <RiEdit2Line onClick={() => setShowEditComment(!showEditComment)} className="edit-comment-icon"/>
+                </div>}
             </div>
+            {showEditComment ?
+            <form onSubmit={handleEditComment} className="update-comment-form">
+                <div>
+                    {validationErrors.map((error, idx) => (
+                        <div className="error-text" key={idx}>{error}</div>
+                    ))}
+                </div>
+                <textarea
+                    className="comment-input"
+                    defaultValue={comment?.content}
+                    name="content"
+                    onChange={e => setContent(e.target.value)}
+                />
+                <div>
+                    <button onClick={() => setShowEditComment(false)} className="cancel-update-comment-button">Cancel</button>
+                    <button type="submit" className="update-comment-button">Update</button>
+                </div>
+            </form>
+            :
             <p className="comment-content">
                 {comment?.content}
             </p>
+            }
         </div>
     )
 }
